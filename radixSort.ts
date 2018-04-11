089316a81ac436c832fe805bcc60b130df909901
Aaron Cronin <cronin@include.cat>
2018-04-11 16:55:04 +0200

    Updated typings

--- radixSort.ts
+++ radixSort.ts
@@ -1,45 +1,76 @@
+// Memoization of Collator creation
+const collatorMap: { [locale: string]: Intl.Collator } = {};
+
+const getCollator = (locale: string) => collatorMap[locale] || (collatorMap[locale] = new Intl.Collator(locale, { sensitivity: "base" }));
+
+// Memoization of comparison function, taking a 3-tuple of locale.string.string
+const comparisonMap: { [locale: string]: { [a: string]: { [b: string]: number } } } = {};
+
+// Wrapper around collator.compare that attempts to reuse memoized comparison
+const compare = (a: string, b: string, locale: string) => {
+    if (!comparisonMap[locale]) {
+        comparisonMap[locale] = {};
+    }
+
+    if (!comparisonMap[locale][a]) {
+        const comparison = getCollator(locale).compare(a, b);
+        comparisonMap[locale][a] = { [b]: comparison };
+        return comparison;
+    } else {
+        return comparisonMap[locale][a][b] || (comparisonMap[locale][a][b] = getCollator(locale).compare(a, b));
+    }
+};
+
+// Used to track algorithm state:
+// * @original: the object to be sorted
+// * @key: the result of mapping the object ot a sortable key
+// * @index: how far the algorithm has progressed when processing characters
+type RadixStateObject<T> = { original: T; key: string; index: number };
+
 // Locale-aware, memoized Radix sorting of input strings
-function radixSort(inputs, locale, toKey){
-  const orderMetadataKey = "_Order_";
-  const endKey = "_End_";
+export function radixSort<T>(inputs: T[], locale: string, toKey: (obj: T) => string): T[] {
+    const endKey = "_End_";
 
-  // Results array is mutated as inner function recursively processes the input
-  const results = new Array(inputs.length);
-  let resultIndex = 0;
+    // Results array is mutated as inner function recursively processes the input
+    const results = new Array(inputs.length);
+    let resultIndex = 0;
 
-  function radixSortInner(inputOrStateObjects, locale, nested) {
-    // Step 1: Collect suffix of strings, bucketed by first character
-    const buckets = {};
-    inputOrStateObjects.forEach(inputOrStateObject => {
-        // inputOrStateObjects is raw on first pass, otherwise state object, e.g. { original: {name: "Dave"}, key: "Dave", index: 1 };
-        const state = nested ? inputOrStateObject : { original: inputOrStateObject, key: toKey(inputOrStateObject), index: 0};
-        const indexedChar = state.key[state.index];
-        const firstChar = indexedChar ? indexedChar : endKey;
-        state.index++;
+    function radixSortInner(inputOrStateObjects: (T | RadixStateObject<T>)[], locale, nested): void {
+        // Step 1: Collect suffix of strings, bucketed by first character
+        const buckets = {};
+        // We iterate through strings (e.g. ["Aaron", "Bob", "Billy"]) and bucket as { "A": ["aron"], "B", ["ob", "illy"]}.
+        inputOrStateObjects.forEach(inputOrStateObject => {
+            // inputOrStateObjects is raw on first pass, otherwise state object, e.g. { original: {name: "Dave"}, key: "Dave", index: 1 };
+            // This is done to avoid an unnecesary extra pass through the data, converting input into the correct state, before processing
+            let state: RadixStateObject<T>;
+            if (nested) {
+                state = inputOrStateObject as RadixStateObject<T>;
+            } else {
+                const input = inputOrStateObject as T;
+                state = { original: input, key: toKey(input), index: 0 };
+            }
 
-        if (!buckets[firstChar]) {
-          buckets[firstChar] = [state];
-        } else {
-          buckets[firstChar].push(state);
-        }
-    });
-    
-    // Step 2: Recursively process each bucket in order, except terminal characters which are emitted.
-    const chars = (buckets[endKey] ? [endKey] : []).concat(
-      Object.keys(buckets)
-        .filter(char => char !== endKey)
-        .sort((a, b) => compare(a, b, locale)))
-  
-    
-    chars.forEach(char => {
-      if (char === endKey) {
-        buckets[endKey].forEach(terminal => results[resultIndex++] = terminal.original)
-      } else {
-        radixSortInner(buckets[char], locale, true)
-      }
-    })
-  }
+            // Grab the character we are processing, using @index from state
+            const character = state.key[state.index] ? state.key[state.index] : endKey;
 
-  radixSortInner(inputs, locale, false);
-  return results;
+            // Update the state so that we are now pointing at the next character in further iterations
+            state.index++;
+
+            if (!buckets[character]) {
+                buckets[character] = [state];
+            } else {
+                buckets[character].push(state);
+            }
+        });
+
+        // Step 2: Recursively process each bucket in order, except terminal characters which are emitted beforehand.
+        (buckets[endKey] || []).forEach(terminal => (results[resultIndex++] = terminal.original));
+        Object.keys(buckets)
+            .filter(char => char !== endKey)
+            .sort((a, b) => compare(a, b, locale))
+            .forEach(char => radixSortInner(buckets[char], locale, true));
+    }
+
+    radixSortInner(inputs, locale, false);
+    return results;
 }
